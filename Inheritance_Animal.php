41c3d49b5714fa69d3a3f328cb5e54d24277fc98
huongvt789 <huongvt52@wru.vn>
2018-04-01 21:13:15 +0700

    add function __construct

--- Inheritance_Animal.php
+++ Inheritance_Animal.php
@@ -6,26 +6,37 @@ class Animal{
 	protected $color;
 	protected $name;
 
+	public function __constructor($weight_animal,$color_animal,$name_animal){
+		$this->weight=$weight_animal;
+		$this->color=$color_animal;
+		$this->name=$name_animal;
+	}
+
 	public function setWeight($weight)
 	{
 		$this->weight=$weight;
 	}
+
 	public function getWeight()
 	{
 		echo $this->weight;
 	}
+
 	public function setColor($color)
 	{
 		$this->color=$color;
 	}
+
 	public function getColor()
 	{
 		echo $this->color;
 	}
+
 	public function setName($name)
 	{
 		$this->name=$name;
 	}
+
 	public function getName(){
 		echo $this->name;
 	}
@@ -40,13 +51,14 @@ class Animal_Dog extends Animal
 	{
 		parent::setWeight();
 	}
+
 	public function getWeight(){
 		parent::getWeight();
 	}
 }
 
 // End to do class Animal_dog
-
+// Call Class Child
  $animal_dog=new Animal;
  $animal_dog->setWeight("100kg");
  $animal_dog->getWeight();
